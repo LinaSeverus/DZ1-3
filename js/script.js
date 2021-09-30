61d56a54c3e492ef497358f7097b33849fc86619
LinaSeverus <severus@ukr.net>
2021-09-30 20:46:20 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Task 1
+let admin;
+const name = 'Джон';
+
+admin = name;
+alert(admin);
+
+// Task 2
+const ourPlanetName = 'Earth';
+let currentUserName;
+
+// Task 3
+let num = '10' + 10;
+console.log(num, typeof num);
